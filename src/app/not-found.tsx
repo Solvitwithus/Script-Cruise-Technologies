bfeb0bf985bab2749c11c0e9f4d29dbbb18d39d2
Solvitwithus <solveitsolution4u@gmail.com>
2025-05-03 22:05:24 +0300

    fixing public

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 // File: app/not-found.tsx
 export default function NotFound() {
     return (
-      <div style={{ textAlign: "center", padding: "4rem" }}>
+      <div style={{ textAlign: "center", padding: "4rem" }} className="text-white">
         <h1>404 - Page Not Found</h1>
         <p>The page you are looking for does not exist.</p>
       </div>
